fccd281bd13be940adde2f31fc77c4ab3abe9901
Lauri Saarela <lauri.saarela@gofore.com>
2023-05-08 14:12:51 +0300

    tailwind config files clean up

--- apps/vf-features/tailwind.config.js
+++ apps/vf-features/tailwind.config.js
@@ -1,12 +1,5 @@
-// tailwind config is required for editor support
-
 const sharedConfig = require('tailwind-config/tailwind.config.js');
 
 module.exports = {
   presets: [sharedConfig],
 };
-
-// content: [
-// 'src/**/*.{js,ts,jsx,tsx}',
-// '../../packages/vf-shared/src/**/*.{js,ts,jsx,tsx}',
-// ]
