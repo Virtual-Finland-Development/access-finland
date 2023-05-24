68791fe2f6412a9e2151284db72840f8070de191
Lauri Saarela <lauri.saarela@gofore.com>
2023-05-24 12:08:41 +0300

    infra code cleanup

--- infra/vf-mvp/index.ts
+++ infra/vf-mvp/index.ts
@@ -71,39 +71,6 @@ const lb = new awsx.lb.ApplicationLoadBalancer(`${projectName}-alb-${env}`, {
   },
 });
 
-/* const defaultVpc = new awsx.ec2.DefaultVpc('default-vpc');
-
-const lb = new aws.lb.LoadBalancer(`${projectName}-alb-${env}`, {
-  loadBalancerType: 'application',
-  subnets: defaultVpc.publicSubnetIds.apply(ids => ids),
-  tags,
-});
-
-const lbTargetGroup = new aws.lb.TargetGroup(`vf-alb-tg-${env}`, {
-  port: 3000,
-  protocol: 'HTTP',
-  vpcId: defaultVpc.vpcId,
-  targetType: 'ip',
-});
-
-const lbListener = new aws.lb.Listener(`${projectName}-alb-listener-${env}`, {
-  loadBalancerArn: lb.arn,
-  port: 80,
-  protocol: 'HTTP',
-  // sslPolicy: '', (needed if https)
-  // certificateArn: '', (needed if https)
-  defaultActions: [
-    {
-      type: 'fixed-response',
-      fixedResponse: {
-        contentType: 'text/plain',
-        messageBody: 'Access denied',
-        statusCode: '403',
-      },
-    },
-  ],
-}); */
-
 const lbListenerRule = new aws.lb.ListenerRule(
   `${projectName}-alb-listener-rule-${env}`,
   {
@@ -113,12 +80,9 @@ const lbListenerRule = new aws.lb.ListenerRule(
           listeners?.find(l => l.port && l.port.apply(p => p === 80))?.arn || ''
         }`
     ),
-    // listenerArn: lbListener.arn,
-    // priority: 100,
     actions: [
       {
         type: 'forward',
-        // targetGroupArn: lbTargetGroup.arn,
         targetGroupArn: lb.defaultTargetGroup.arn,
       },
     ],
@@ -148,7 +112,6 @@ const fargateService = new awsx.ecs.FargateService(
           portMappings: [
             {
               targetGroup: lb.defaultTargetGroup,
-              // targetGroup: lbTargetGroup,
             },
           ],
         },
@@ -171,8 +134,6 @@ const cdn = new aws.cloudfront.Distribution(
       {
         originId: lb.loadBalancer.arn,
         domainName: lb.loadBalancer.dnsName,
-        /* originId: lb.arn,
-        domainName: lb.dnsName, */
         customOriginConfig: {
           originProtocolPolicy: 'http-only',
           originSslProtocols: ['TLSv1.2'],
@@ -189,7 +150,6 @@ const cdn = new aws.cloudfront.Distribution(
     ],
     defaultCacheBehavior: {
       targetOriginId: lb.loadBalancer.arn,
-      // targetOriginId: lb.arn,
       viewerProtocolPolicy: 'redirect-to-https',
       allowedMethods: ['GET', 'HEAD', 'OPTIONS'],
       cachedMethods: ['GET', 'HEAD', 'OPTIONS'],
@@ -220,15 +180,5 @@ const cdn = new aws.cloudfront.Distribution(
 
 // Export the URL of load balancer.
 export const url = lb.loadBalancer.dnsName;
-// export const url = lb.dnsName;
 // Export the CloudFront url.
 export const cdnURL = pulumi.interpolate`https://${cdn.domainName}`;
-
-export const listenerArn = lb.listeners.apply(
-  listeners =>
-    pulumi.interpolate`${
-      listeners?.find(l => l.port && l.port.apply(p => p === 80))?.arn || ''
-    }`
-);
-
-// export const vpcPublicSubnetIds = defaultVpc.publicSubnetIds.apply(ids => ids);
