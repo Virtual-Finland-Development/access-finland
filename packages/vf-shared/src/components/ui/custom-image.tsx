ae6d1efbb9f299749be674e2de91231111a701b1
Lauri Saarela <lauri.saarela@gofore.com>
2023-05-25 15:19:26 +0300

    image module: flip the env variable to check if exported app (vf-features) instead of checking if app with server (vf-mvp), since with server is the 'normal way' for next apps

--- packages/vf-shared/src/components/ui/custom-image.tsx
+++ packages/vf-shared/src/components/ui/custom-image.tsx
@@ -6,14 +6,14 @@ import { ImageProps } from 'next/image';
  * 'next/image' can't be used with exported apps for image optimization, use 'next-image-export-optimizer' module instead.
  * 'next-image-export-optimizer' has its custom loader configs in apps/vf-features/next.config.js.
  */
-const ImageComponent = dynamic(() => {
-  const withServer = process.env.NEXT_PUBLIC_WITH_SERVER || false;
+const ImageComponent = dynamic<ImageProps>(() => {
+  const isExport = process.env.NEXT_PUBLIC_IS_EXPORT || false;
 
-  if (withServer) {
-    return import('next/image').then(mod => mod);
+  if (isExport) {
+    return import('next-image-export-optimizer').then(mod => mod);
   }
 
-  return import('next-image-export-optimizer').then(mod => mod);
+  return import('next/image').then(mod => mod);
 });
 
 export default function CustomImage(props: ImageProps) {
