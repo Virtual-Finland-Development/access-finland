0fecb84338f5d6dfde6c3cb25437a136bbb1332b
Lassi Piironen <lsipii@kapsi.fi>
2023-06-16 16:32:33 +0300

    chore: logged-off page initial loading state, rearranged the info texts

--- apps/vf-mvp/src/pages/logged-out.page.tsx
+++ apps/vf-mvp/src/pages/logged-out.page.tsx
@@ -14,12 +14,11 @@ function getSinunaServiceUrl() {
 }
 
 export default function LoggedOutPage() {
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
   const router = useRouter();
   const sinunaLoginServiceLink = getSinunaServiceUrl();
   const { initiator } = router.query;
   useEffect(() => {
-    setLoading(true);
     if (router.isReady) {
       if (initiator === 'auth-service') {
         window.history.replaceState(null, '', '/logged-out'); // Clear query params without redrawing
@@ -41,10 +40,8 @@ export default function LoggedOutPage() {
         <Alert status="neutral" labelText="Logged out from Virtual Finland!">
           <div className="flex flex-col gap-3">
             <Text>
-              Logging out from Virtual Finland succeeded. Note that the Sinuna
-              login session might still be active.
+              Note that the Sinuna login session might still be active.
             </Text>
-            <CustomLink href="/">Go to home page</CustomLink>
             <Text>Manage Sinuna login at:</Text>
             <Link
               target="_blank"
@@ -53,6 +50,8 @@ export default function LoggedOutPage() {
             >
               Sinuna Service
             </Link>
+
+            <CustomLink href="/">Go to home page</CustomLink>
           </div>
         </Alert>
       </div>
