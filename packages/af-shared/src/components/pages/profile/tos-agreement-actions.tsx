0a7a5266987223bc938becf5b7aa586fd0cf5fc1
Lauri Saarela <lauri.saarela@gofore.com>
2023-09-06 14:28:21 +0300

    tos-agreement-actions: more dry implementation for variants

--- packages/af-shared/src/components/pages/profile/tos-agreement-actions.tsx
+++ packages/af-shared/src/components/pages/profile/tos-agreement-actions.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, ReactNode, useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { Button, InlineAlert, Text } from 'suomifi-ui-components';
 import api from '@/lib/api';
@@ -11,102 +11,44 @@ import DangerButton from '@/components/ui/danger-button';
 import Loading from '@/components/ui/loading';
 import ProfileDeleteConfirmation from './profile-details/profile-delete-confirmation';
 
+const TITLE = {
+  new: 'Terms of Use Agreement',
+  change: 'Updated Terms of Use Agreement',
+};
+const LINK_TITLE = {
+  new: 'Access Finland Terms of Use',
+  change: 'Updated Terms of Use',
+};
+const CONTENT_1 = {
+  new: 'Before you can create a digital profile and access our services, please take a moment to read and accept our Terms of Use. These terms are important as they outline the rules and guidelines for using our platform. By accepting these terms, you agree to abide by them throughout your usage of our services.',
+  change:
+    'Before you can continue to access our services, we want to inform you that we have recently updated our Terms of Use. These updated terms are important as they outline the rules and guidelines for using our platform. By continuing to use our services, you agree to abide by these updated terms.',
+};
+const CONTENT_2 = {
+  new: 'By clicking "Accept," you acknowledge that you have read and understood our Terms of Use Agreement. You also consent to the collection, use, and storage of your personal information as outlined in our Privacy Policy. If you do not agree with these terms, please do not create a profile on Access Finland.',
+  change:
+    'By clicking "Accept," you acknowledge that you have read and understood our Updated Terms of Use Agreement. You also consent to the collection, use, and storage of your personal information as outlined in our Privacy Policy. If you do not agree with these updated terms, you are able to delete your Access Finland profile and stop using the service.',
+};
+
 interface AgreementProps {
-  onAccept: () => void;
-  onCancel: () => void;
-  isLoading: boolean;
+  contentKey: 'new' | 'change';
+  children?: ReactNode;
 }
 
-function Agreement(props: AgreementProps) {
-  const { onAccept, onCancel, isLoading } = props;
+function AgreementContent(props: AgreementProps) {
+  const { contentKey, children } = props;
 
   return (
     <div className="flex flex-col items-start gap-6">
-      <CustomHeading variant="h3">Tems of Use Agreement</CustomHeading>
-      <Text>
-        Before you can create a user account and access our services, please
-        take a moment to read and accept our Terms of Use. These terms are
-        important as they outline the rules and guidelines for using our
-        platform. By accepting these terms, you agree to abide by them
-        throughout your usage of our services.
-      </Text>
+      <CustomHeading variant="h3">{TITLE[contentKey]}</CustomHeading>
+      <Text>{CONTENT_1[contentKey]}</Text>
       <Text>
         <CustomLink href="/terms-of-use" target="_blank" disableVisited>
-          Access Finland Terms of Use
+          {LINK_TITLE[contentKey]}
         </CustomLink>
       </Text>
-      <Text>
-        By clicking &quot;Accept,&quot; you acknowledge that you have read and
-        understood our Terms of Use Agreement. You also consent to the
-        collection, use, and storage of your personal information as outlined in
-        our Privacy Policy. If you do not agree with these terms, please do not
-        create an account on Access Finland.
-      </Text>
-      <div className="flex flex-row items-center gap-3">
-        <Button onClick={onAccept} disabled={isLoading}>
-          Accept
-        </Button>
-        <Button variant="secondary" onClick={onCancel} disabled={isLoading}>
-          I don’t accept, sign out
-        </Button>
-        {isLoading && (
-          <div className="mt-1 ml-1">
-            <Loading variant="small" />
-          </div>
-        )}
-      </div>
-    </div>
-  );
-}
-
-type AgreementChangeProps = Omit<AgreementProps, 'onCancel'>;
-
-function AgreementChange(props: AgreementChangeProps) {
-  const { onAccept, isLoading } = props;
-  const { openModal, closeModal } = useModal();
-
-  const onDelete = () =>
-    openModal({
-      title: 'Delete profile',
-      content: <ProfileDeleteConfirmation onCancel={closeModal} />,
-      onClose: closeModal,
-      closeOnEsc: false,
-    });
-
-  return (
-    <div className="flex flex-col items-start gap-6">
-      <CustomHeading variant="h3">Updated Terms of Use Agreement</CustomHeading>
-      <Text>
-        Before you can continue to access our services, we want to inform you
-        that we have recently updated our Terms of Use. These updated terms are
-        important as they outline the rules and guidelines for using our
-        platform. By continuing to use our services, you agree to abide by these
-        updated terms.
-      </Text>
-      <CustomLink href="/terms-of-use" target="_blank" disableVisited>
-        Updated Terms of Use
-      </CustomLink>
-      <Text>
-        By clicking &quot;Accept,&quot; you acknowledge that you have read and
-        understood our Updated Terms of Use Agreement. You also consent to the
-        collection, use, and storage of your personal information as outlined in
-        our Privacy Policy. If you do not agree with these updated terms, you
-        are able to delete your Access Finland profile and stop using the
-        service.
-      </Text>
-      <div className="flex flex-row items-center gap-3">
-        <Button onClick={onAccept} disabled={isLoading}>
-          Accept
-        </Button>
-        <DangerButton onClick={onDelete} disabled={isLoading}>
-          I don’t accept, delete my profile
-        </DangerButton>
-        {isLoading && (
-          <div className="mt-1 ml-1">
-            <Loading variant="small" />
-          </div>
-        )}
-      </div>
+      <Text>{CONTENT_2[contentKey]}</Text>
+      <div className="flex flex-row items-center gap-3">{children}</div>
     </div>
   );
 }
@@ -119,6 +61,7 @@ export default function TosAgreementActions(props: Props) {
   const { isNewUser } = props;
   const reactQueryClient = useQueryClient();
   const toast = useToast();
+  const { openModal, closeModal } = useModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const onAccept = async () => {
@@ -138,22 +81,43 @@ export default function TosAgreementActions(props: Props) {
     }
   };
 
-  const onCancel = async () => {
-    setIsLoading(true);
-    await api.auth.directToAuthLogout();
+  const onDeny = async () => {
+    if (isNewUser) {
+      setIsLoading(true);
+      await api.auth.directToAuthLogout();
+    } else {
+      openModal({
+        title: 'Delete profile',
+        content: <ProfileDeleteConfirmation onCancel={closeModal} />,
+        onClose: closeModal,
+        closeOnEsc: false,
+      });
+    }
   };
 
   return (
     <InlineAlert status={isNewUser ? 'neutral' : 'warning'}>
-      {isNewUser ? (
-        <Agreement
-          onAccept={onAccept}
-          onCancel={onCancel}
-          isLoading={isLoading}
-        />
-      ) : (
-        <AgreementChange onAccept={onAccept} isLoading={isLoading} />
-      )}
+      <AgreementContent contentKey={isNewUser ? 'new' : 'change'}>
+        <Fragment>
+          <Button onClick={onAccept} disabled={isLoading}>
+            Accept
+          </Button>
+          {isNewUser ? (
+            <Button variant="secondary" onClick={onDeny} disabled={isLoading}>
+              I don’t accept
+            </Button>
+          ) : (
+            <DangerButton onClick={onDeny} disabled={isLoading}>
+              I don’t accept, delete my profile
+            </DangerButton>
+          )}
+          {isLoading && (
+            <div className="mt-1 ml-1">
+              <Loading variant="small" />
+            </div>
+          )}
+        </Fragment>
+      </AgreementContent>
     </InlineAlert>
   );
 }
