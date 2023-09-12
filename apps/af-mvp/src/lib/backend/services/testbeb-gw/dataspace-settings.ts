ed2d390fb3897ce3c15c23a8024141184f26cc16
Lassi Piironen <lsipii@kapsi.fi>
2023-09-12 15:36:46 +0300

    feat: allow test-routes passthrough

--- apps/af-mvp/src/lib/backend/services/testbeb-gw/dataspace-settings.ts
+++ apps/af-mvp/src/lib/backend/services/testbeb-gw/dataspace-settings.ts
@@ -4,8 +4,12 @@ export const schemaVersion = 'v0.1';
 
 export function getDataProductRoutePath(dataProduct: DataProduct) {
   const stage = process.env.NEXT_PUBLIC_STAGE;
-  if (stage === 'mvp-staging') {
+  if (stage === 'mvp-staging' || stage === 'mvp-production') {
     return `${dataProduct}_${schemaVersion}`;
+  }
+
+  if (dataProduct.startsWith('test/')) {
+    return dataProduct;
   }
   return `Draft/${dataProduct}`;
 }
