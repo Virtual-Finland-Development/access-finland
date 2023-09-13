a7ccb8f1dafffe6775b111c78eae8f1d3887312d
Lassi Piironen <lsipii@kapsi.fi>
2023-09-13 09:47:55 +0300

    fix: allow test dataproduct-defs to run in mvp-staging

--- apps/af-mvp/src/lib/backend/services/testbeb-gw/dataspace-settings.ts
+++ apps/af-mvp/src/lib/backend/services/testbeb-gw/dataspace-settings.ts
@@ -3,13 +3,13 @@ import { DataProduct } from 'af-shared/src/types';
 export const schemaVersion = 'v0.1';
 
 export function getDataProductRoutePath(dataProduct: DataProduct) {
+  if (dataProduct.startsWith('test/')) {
+    return dataProduct;
+  }
+
   const stage = process.env.NEXT_PUBLIC_STAGE;
   if (stage === 'mvp-staging' || stage === 'mvp-production') {
     return `${dataProduct}_${schemaVersion}`;
-  }
-
-  if (dataProduct.startsWith('test/')) {
-    return dataProduct;
   }
   return `Draft/${dataProduct}`;
 }
