8366635e6186045b1b29de9ec3de12965a773152
Lauri Saarela <lauri.saarela@gofore.com>
2023-11-14 14:45:30 +0200

    more mocks

--- apps/af-features/src/pages/profile/residence-permits.page.tsx
+++ apps/af-features/src/pages/profile/residence-permits.page.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { MdCancel, MdDone, MdOutlineInfo } from 'react-icons/md';
+import migriLogo from '@shared/images/MIGRI_logo.svg';
 import {
   Expander,
   ExpanderContent,
@@ -10,8 +11,22 @@ import {
 import AuthSentry from '@shared/components/auth-sentry';
 import Page from '@shared/components/layout/page';
 import CustomHeading from '@shared/components/ui/custom-heading';
+import CustomImage from '@shared/components/ui/custom-image';
 import Loading from '@shared/components/ui/loading';
 import PageSideNavLayout from './components/profile-side-nav-layout';
+
+const STATUS_TEXTS = {
+  completed: 'The residence permit application has been completed.',
+  pending: "The residence permit application hasn't been processed yet.",
+  rejected:
+    'The residence permit application has been rejected. Reason: lorem ipsum dolor sit amet.',
+};
+
+const STATUS_VARIANT = {
+  completed: 'success',
+  pending: 'warning',
+  rejected: 'error',
+};
 
 const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
 
@@ -23,6 +38,23 @@ const MOCK_DATA = async () => {
     { id: 3, issuer: 'Migri', type: 'P', status: 'rejected' },
   ];
 };
+
+function renderIcon(status: string) {
+  switch (status) {
+    case 'completed':
+      return <MdDone size={22} color="green" />;
+    case 'pending':
+      return <MdOutlineInfo size={22} color="orange" />;
+    case 'rejected':
+      return <MdCancel size={22} color="red" />;
+    default:
+      return <MdOutlineInfo size={22} color="orange" />;
+  }
+}
+
+function capitalize(string: string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default function ResidencePermitsPage() {
   const [isLoading, setIsLoading] = useState(true);
@@ -48,7 +80,7 @@ export default function ResidencePermitsPage() {
 
   return (
     <AuthSentry redirectPath="/profile">
-      <PageSideNavLayout title="Work permits">
+      <PageSideNavLayout title="Residence permits">
         <Page.Block className="bg-white">
           <div className="flex flex-col gap-6 items-start">
             <div className="flex flex-row items-center">
@@ -76,38 +108,75 @@ export default function ResidencePermitsPage() {
                     </ExpanderTitleButton>
                     <ExpanderContent className="!text-base !flex !flex-col !gap-4">
                       <div className="flex flex-col gap-4">
-                        <Text>1. Issuer: {permit.issuer}</Text>
-                        <Text>2. Type: {permit.type}</Text>
-                        <Text>3. Status: {permit.status}</Text>
-                      </div>
-                      {permit.status === 'rejected' && (
-                        <InlineAlert status="error" labelText="Rejected">
-                          <Text>
-                            The residence permit application has been rejected.
-                          </Text>
-                        </InlineAlert>
-                      )}
-                      <InlineAlert labelText="Permit type">
                         <Text>
-                          1. Permit type, or the grounds for issuing the permit:
+                          1. Issuer:{' '}
+                          <span className="font-semibold">{permit.issuer}</span>
                         </Text>
-                        <ul className="list-outside list-disc ms-8">
-                          <li>
-                            <Text>A = continuous residence permit</Text>
-                          </li>
-                          <li>
-                            <Text>B = temporary residence permit</Text>
-                          </li>
-                          <li>
-                            <Text>P = permanent residence permit</Text>
-                          </li>
-                          <li>
-                            <Text>
-                              P-EU or P-EY = a long-term resident’s EU residence
-                              permit issued to a third-country national.
-                            </Text>
-                          </li>
-                        </ul>
+                        <CustomImage src={migriLogo} alt="Migri" height={100} />
+                        <Text>
+                          2. Permit type:{' '}
+                          <span className="font-semibold">{permit.type}</span>
+                        </Text>
+                        <InlineAlert labelText="Permit type">
+                          <Text>
+                            1. Permit type, or the grounds for issuing the
+                            permit:
+                          </Text>
+                          <ul className="list-outside list-disc ms-8">
+                            <li>
+                              <Text
+                                className={
+                                  permit.type === 'A'
+                                    ? '!font-bold'
+                                    : 'font-normal'
+                                }
+                              >
+                                A = continuous residence permit
+                              </Text>
+                            </li>
+                            <li>
+                              <Text
+                                className={
+                                  permit.type === 'B'
+                                    ? '!font-bold'
+                                    : 'font-normal'
+                                }
+                              >
+                                B = temporary residence permit
+                              </Text>
+                            </li>
+                            <li>
+                              <Text
+                                className={
+                                  permit.type === 'P'
+                                    ? '!font-bold'
+                                    : 'font-normal'
+                                }
+                              >
+                                P = permanent residence permit
+                              </Text>
+                            </li>
+                            <li>
+                              <Text>
+                                P-EU or P-EY = a long-term resident’s EU
+                                residence permit issued to a third-country
+                                national.
+                              </Text>
+                            </li>
+                          </ul>
+                        </InlineAlert>
+                        <Text>
+                          3. Status:{' '}
+                          <span className="font-semibold">
+                            {capitalize(permit.status)}
+                          </span>
+                        </Text>
+                      </div>
+                      <InlineAlert
+                        status={STATUS_VARIANT[permit.status]}
+                        labelText={capitalize(permit.status)}
+                      >
+                        <Text>{STATUS_TEXTS[permit.status]}</Text>
                       </InlineAlert>
                     </ExpanderContent>
                   </Expander>
@@ -119,17 +188,4 @@ export default function ResidencePermitsPage() {
       </PageSideNavLayout>
     </AuthSentry>
   );
-}
-
-function renderIcon(status: string) {
-  switch (status) {
-    case 'completed':
-      return <MdDone size={22} color="green" />;
-    case 'pending':
-      return <MdOutlineInfo size={22} color="orange" />;
-    case 'rejected':
-      return <MdCancel size={22} color="red" />;
-    default:
-      return <MdOutlineInfo size={22} color="orange" />;
-  }
 }
--- apps/af-features/src/pages/profile/taxation.page.tsx
+++ apps/af-features/src/pages/profile/taxation.page.tsx
@@ -1,12 +1,106 @@
+import { useEffect, useState } from 'react';
+import veroLogo from '@shared/images/logo-vero.svg';
+import { format, parseISO } from 'date-fns';
+import { Text } from 'suomifi-ui-components';
 import AuthSentry from '@shared/components/auth-sentry';
 import Page from '@shared/components/layout/page';
+import CustomHeading from '@shared/components/ui/custom-heading';
+import CustomImage from '@shared/components/ui/custom-image';
+import Loading from '@shared/components/ui/loading';
 import PageSideNavLayout from './components/profile-side-nav-layout';
 
+const sleep = () => new Promise(resolve => setTimeout(resolve, 1000));
+
+const DATA = {
+  taxRate: 15.5,
+  incomeLimitYear: 30000,
+  incomeLimitRestOfYear: 27000,
+  validityStart: '2023-01-01',
+  validityEnd: '2023-12-31',
+};
+
+const MOCK_DATA = async () => {
+  await sleep();
+  return DATA;
+};
+
+function formatEuro(num) {
+  return new Intl.NumberFormat('de-DE', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format(num);
+}
+
 export default function TaxationPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<typeof DATA | undefined>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await MOCK_DATA();
+        setData(response);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <AuthSentry redirectPath="/profile">
       <PageSideNavLayout title="Taxation">
-        <Page.Block className="bg-white">Taxation Page</Page.Block>
+        <Page.Block className="bg-white">
+          <div className="flex flex-col gap-6 items-start">
+            <div className="flex flex-row items-center">
+              <CustomHeading variant="h2" suomiFiBlue="dark">
+                Your Taxation
+              </CustomHeading>
+            </div>
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam a
+              diam eget felis aliquam dignissim. Sed euismod, nisl eget aliquam
+              ultricies, nunc nisl ultricies nunc, quis ultricies nisl nisl
+              vitae nunc.
+            </Text>
+
+            {isLoading ? (
+              <Loading />
+            ) : (
+              <div className="flex flex-col gap-4">
+                <Text>
+                  Tax rate:{' '}
+                  <span className="block font-semibold">{data!.taxRate} %</span>
+                </Text>
+                <Text>
+                  Income limit for the whole year:{' '}
+                  <span className="block font-semibold">
+                    {formatEuro(data!.incomeLimitYear)}
+                  </span>
+                </Text>
+                <Text>
+                  Income limit for the rest of the year:{' '}
+                  <span className="block font-semibold">
+                    {formatEuro(data!.incomeLimitRestOfYear)}
+                  </span>
+                </Text>
+                <Text>
+                  Validity:{' '}
+                  <span className="block font-semibold">
+                    {format(parseISO(data!.validityStart), 'dd.MM.yyyy')} -{' '}
+                    {format(parseISO(data!.validityEnd), 'dd.MM.yyyy')}
+                  </span>
+                </Text>
+
+                <CustomImage src={veroLogo} alt="Vero" />
+              </div>
+            )}
+          </div>
+        </Page.Block>
       </PageSideNavLayout>
     </AuthSentry>
   );
