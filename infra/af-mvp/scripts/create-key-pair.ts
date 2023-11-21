01f0763190f686d71af5e7f3fffd5b4e7fbff92c
Lassi Piironen <lsipii@kapsi.fi>
2023-11-21 10:16:12 +0200

    chore: cleanup

--- infra/af-mvp/scripts/create-key-pair.ts
+++ infra/af-mvp/scripts/create-key-pair.ts
@@ -1,9 +1,20 @@
-import { generateKeyPair } from '../utils/rsa';
+// npx ts-node create-key-pair.ts
+import { generateKeyPairSync } from 'crypto';
+
+function generateKeyPair() {
+  return generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: {
+      type: 'spki',
+      format: 'pem',
+    },
+    privateKeyEncoding: {
+      type: 'pkcs8',
+      format: 'pem',
+    },
+  });
+}
 
 const keyPair = generateKeyPair();
-console.log(`> Public key: 
-${keyPair.publicKey}
-`);
-console.log(`> Private key:
-${keyPair.privateKey}
-`);
+console.log(keyPair.publicKey);
+console.log(keyPair.privateKey);
