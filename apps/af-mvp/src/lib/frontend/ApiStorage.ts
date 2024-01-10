978645019916086e74beb5b7ce72d8dcc4f49b76
Lassi Piironen <lsipii@kapsi.fi>
2024-01-10 12:39:18 +0200

    feat: add local in-memory cache for the ApiStorage to reduce the requests in case the same info requested many times

--- apps/af-mvp/src/lib/frontend/ApiStorage.ts
+++ apps/af-mvp/src/lib/frontend/ApiStorage.ts
@@ -5,6 +5,7 @@ import apiClient from '@shared/lib/api/api-client';
 export class ApiStorage implements KeyValueStorageInterface {
   private apiClient: AxiosInstance;
   private endpoint: string;
+  private cache: Record<string, string> = {};
 
   constructor(endpoint: string) {
     this.endpoint = endpoint;
@@ -12,9 +13,13 @@ export class ApiStorage implements KeyValueStorageInterface {
   }
 
   async setItem(key: string, value: string): Promise<void> {
+    this.cache[key] = value;
     await this.apiClient.post(this.endpoint, { action: 'set', key, value });
   }
   async getItem(key: string): Promise<string | null> {
+    if (typeof this.cache[key] !== 'undefined') {
+      return this.cache[key];
+    }
     const response = await this.apiClient.post(this.endpoint, {
       action: 'get',
       key,
@@ -22,9 +27,11 @@ export class ApiStorage implements KeyValueStorageInterface {
     return response.data.value;
   }
   async removeItem(key: string): Promise<void> {
+    delete this.cache[key];
     await this.apiClient.post(this.endpoint, { action: 'remove', key });
   }
   async clear(): Promise<void> {
+    this.cache = {};
     await this.apiClient.post(this.endpoint, { action: 'clear' });
   }
 }
