b90a2c67f2f6786a27242658655ee3005bca6c93
Lauri Saarela <lauri.saarela@gofore.com>
2024-01-11 09:46:47 +0200

    Loading component - add possibility to use as overlay loading + small tweaks to indicator positioning

--- packages/af-shared/src/components/ui/loading.tsx
+++ packages/af-shared/src/components/ui/loading.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from 'react';
 import { LoadingSpinner } from 'suomifi-ui-components';
 
 interface Props {
@@ -5,6 +6,10 @@ interface Props {
   text?: string;
   textAlign?: 'bottom' | 'right';
   variant?: 'normal' | 'small';
+  /** When used, loading indicator is absolute positioned relative to container element. Add 'position: relative' to container element when needed. */
+  asOverlay?: boolean;
+  /** Background color for the loading overlay. Defaults to 'white'. Use valid CSS color value like HEX, RGB or corresponding string value. */
+  overlayBgColor?: string;
 }
 
 export default function Loading(props: Props) {
@@ -13,10 +18,27 @@ export default function Loading(props: Props) {
     text = '',
     textAlign = undefined,
     variant = 'normal',
+    asOverlay = false,
+    overlayBgColor = 'white',
   } = props;
 
-  return (
+  const renderLoadingSpinner = (spinner: ReactNode) => {
+    if (asOverlay) {
+      return (
+        <div
+          className={`absolute inset-0 flex items-center justify-center bg-[${overlayBgColor}] bg-opacity-60 z-10`}
+        >
+          {spinner}
+        </div>
+      );
+    }
+
+    return spinner;
+  };
+
+  return renderLoadingSpinner(
     <LoadingSpinner
+      className="!leading-none"
       status={status}
       text={text}
       textAlign={textAlign}
