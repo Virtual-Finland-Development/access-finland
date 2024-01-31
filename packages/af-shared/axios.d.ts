ae089d38120dbfb6e14752bfecdca0d0c6366845
Lauri Saarela <lauri.saarela@gofore.com>
2024-01-31 14:07:44 +0200

    include axios declaration file for both apps

--- packages/af-shared/axios.d.ts
+++ packages/af-shared/axios.d.ts
@@ -4,7 +4,7 @@ import 'axios';
  * Extend AxiosRequestConfig interface
  */
 declare module 'axios' {
-  export interface AxiosRequestConfig {
+  interface AxiosRequestConfig {
     idTokenRequired?: boolean; // tells if 'Auhtorization' with bearer idtoken is required for the request headers
     csrfTokenRequired?: boolean; // tells if 'x-csrf-token' is required for the request headers (af-mvp only)
   }
