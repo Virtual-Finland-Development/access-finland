957d4f9bc7463109606f2e7940c87a058c6c4bca
Lauri Saarela <lauri.saarela@gofore.com>
2024-01-31 15:39:24 +0200

    simplify previous interceptors.response.error handler

--- packages/af-shared/src/lib/api/api-client.ts
+++ packages/af-shared/src/lib/api/api-client.ts
@@ -1,4 +1,4 @@
-import axios, { isAxiosError } from 'axios';
+import axios, { AxiosError, isAxiosError } from 'axios';
 import { REQUEST_NOT_AUTHORIZED } from '../constants';
 import { isExportedApplication } from '../utils';
 import { getValidAuthState } from '../utils/auth';
@@ -52,24 +52,21 @@ let tokenExpirationAlertDisplayed = false;
 apiClient.interceptors.response.use(
   response => response,
   async error => {
-    if (isAxiosError(error)) {
-      // either no token, or it has expired
-      const hasExpired = !(await getValidAuthState()).isValid;
+    // either no token, or it has expired
+    const hasExpired = !(await getValidAuthState()).isValid;
 
-      if (
-        (error.config?.idTokenRequired || error.config?.csrfTokenRequired) &&
-        hasExpired
-      ) {
-        if (!tokenExpirationAlertDisplayed) {
-          tokenExpirationAlertDisplayed = true;
-          window.postMessage(REQUEST_NOT_AUTHORIZED);
-        }
-
-        // essentially, silence the error for token expiration cases for UI
-        return new Promise(() => {});
-      } else {
-        return Promise.reject(error);
+    if (
+      isAxiosError(error) &&
+      (error.config?.idTokenRequired || error.config?.csrfTokenRequired) &&
+      hasExpired
+    ) {
+      if (!tokenExpirationAlertDisplayed) {
+        tokenExpirationAlertDisplayed = true;
+        window.postMessage(REQUEST_NOT_AUTHORIZED);
       }
+
+      // essentially, silence the error for token expiration cases for UI
+      return new Promise(() => {});
     }
 
     return Promise.reject(error);
